10d6178d57cae6fc336737a7420068d1e553c6f1
facundo <facundoq654@gmail.com>
2025-07-11 00:43:16 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-red-100 text-center p-4">
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+export default App;
